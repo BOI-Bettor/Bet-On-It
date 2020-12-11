eb63b8e8b6f2e7faca5bc6433932f1e933aa648d
Jamee Cody <jamee3059@yahoo.com>
2020-12-11 01:49:54 -0500

    Tried to make a basic table with dummy data, but it's in an activity

--- app/src/main/java/com/example/betonit_bettor/BHFragment.java
+++ app/src/main/java/com/example/betonit_bettor/BHFragment.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class BHFragment extends Fragment {
+    TableLayout bh_table;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -17,5 +19,8 @@ public class BHFragment extends Fragment {
         getActivity().setTitle("Betting History");
 
         return inflater.inflate(R.layout.fragment_bettinghistory, container, false);
+
+
+
     }
 }
